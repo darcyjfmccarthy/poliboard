1317a00a31b68fc49cd6cc347e03675652406a23
Darcy McCarthy <darcyjfmccarthy@gmail.com>
2025-01-12 18:58:41 +1100

    CHORE: Added GHP to cors origins

--- backend/main.py
+++ backend/main.py
@@ -16,6 +16,7 @@ origins = [
     "http://localhost:8080",
     "http://127.0.0.1",
     "http://127.0.0.1:8000",
+    "https://darcyjfmccarthy.github.io/poliboard",
     # Add your frontend URL when you deploy it
 ]
 
